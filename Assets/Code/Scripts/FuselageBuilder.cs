adbb0a78c15c029ad9ee8ba4b3c8b91e4ab05ec9
Leonard Pak <leopak2000@gmail.com>
2023-09-01 16:20:49 +0300

    Add cylindrical plane script

--- Assets/Code/Scripts/FuselageBuilder.cs
+++ Assets/Code/Scripts/FuselageBuilder.cs
@@ -19,11 +19,7 @@ namespace SimulatorRivetingRoboticArm
         [SerializeField] private float xMax = 0f;
         [SerializeField, Range(0f, 360f)] protected float angleMax = 0f;
 
-        private double alpha = 0f;
-        private double beta = 0f;
-
-        private Vector3 angleStep = Vector3.zero;
-        private Vector3 angle0 = Vector3.zero;
+        private CylindricalPlaneCalculator plane = null;
 
         public int MtxDimX => (int)(xMax / sideSize);
         public int MtxDimY => (int)((math.PI * R * angleMax / 180) / sideSize);
@@ -32,34 +28,10 @@ namespace SimulatorRivetingRoboticArm
          * iter - current iteration, start at 0
          * returns the increment to be made at the current step 
          */
-        private double XPlaneIterationInc(int iter)
-        {
-            if (iter == 0) return sideSize / 2;
-            return sideSize;
-        }
-        private double YPlaneIterationInc(int iter)
-        {
-            if (iter == 0) return sideSize * math.cos(alpha / 2) / 2;
-            if (iter == 1) return sideSize * (math.cos(alpha / 2) + math.cos(alpha / 2 + iter * alpha)) / 2;
-            return sideSize * (math.cos(alpha / 2 + iter * alpha) + math.cos(alpha / 2 + (iter - 1) * alpha)) / 2;
-        }
-        private double ZPlaneIterationInc(int iter)
-        {
-            if (iter == 0) return -R + sideSize * math.sin(alpha / 2) / 2;
-            if (iter == 1) return sideSize * (math.sin(alpha / 2) + math.cos(beta - alpha)) / 2;
-            return sideSize * (math.cos(beta - iter * alpha) + math.cos(beta - (iter - 1) * alpha)) / 2;
-        }
-        private Vector3 AngleIterationInc(int iter)
-        {
-            if (iter == 0) return blockType1.transform.rotation.eulerAngles + angle0;
-            return angleStep;
-        }
+
         private void Awake()
         {
-            alpha = math.acos(1 - (math.pow(sideSize, 2) / (2 * math.pow(R, 2))));
-            beta = (math.PI_DBL - alpha) / 2;
-            angleStep.Set(0f, 0f, (float)math.degrees(alpha));
-            angle0.Set(0f, 0f, (float)math.degrees(alpha / 2));
+            plane = new CylindricalPlaneCalculator(R, sideSize, blockType1.transform.eulerAngles);
         }
         public void Build(Matrix2D binaryMtx)
         {
@@ -69,14 +41,14 @@ namespace SimulatorRivetingRoboticArm
             var z = 0d;
             for (int i = 0; i < binaryMtx.Count; ++i)
             {
-                y += YPlaneIterationInc(i);
-                z += ZPlaneIterationInc(i);
-                rotation.eulerAngles += AngleIterationInc(i);
+                y += plane.YPlaneIterationInc(i);
+                z += plane.ZPlaneIterationInc(i);
+                rotation.eulerAngles += plane.AngleIterationInc(i);
                 var x = 0d;
 
                 for (int j = 0; j < binaryMtx[i].Count; ++j)
                 {
-                    x += XPlaneIterationInc(j);
+                    x += plane.XPlaneIterationInc(j);
                     position.Set((float)x, (float)y, (float)z);
                     var block = Instantiate(
                         (binaryMtx[i][j]) ? blockType1 : blockType2,
